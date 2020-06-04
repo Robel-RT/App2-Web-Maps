2ea1023fa115a6448529152b8d27db353b547a51
Robel-RT <robelasgedom92@gmail.com>
2020-06-04 15:41:34 +0100

    added lat, lon data

--- webbranch.py
+++ webbranch.py
@@ -1,10 +1,20 @@
 import folium
+import pandas
+
+data = pandas.read_csv("volcanoes.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
 
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
 
-fg=folium.FeatureGroup(name="My Group")
-fg.add_child(folium.Marker(location=[38.2, -99.1], popup="Hi I am a Marker!", icon=folium.Icon(color='green')))
+fg=folium.FeatureGroup(name="My Map")
+
+for lt, ln in zip(lat, lon):
+    fg.add_child(folium.Marker(location=[lt, ln], popup="Hi I am a Marker!", icon=folium.Icon(color='green'))
+
+
 map.add_child(fg)
 
 map.save("Map1.html")
+
 
