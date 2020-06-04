46df5f97a2b995c7a6dfc835151853330196816a
Robel-RT <robelasgedom92@gmail.com>
2020-06-04 13:51:02 +0100

    modify Map2.html

--- webbranch.py
+++ webbranch.py
@@ -5,4 +5,5 @@ map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Mapbox Bright")
 map.add_child(folium.Marker(location=[38.2, -99.1], popup="Hi I am a Marker!", icon=folium.Icon(color='green')))
 
 
-map.save("Map1.html")
+map.save("Map2.html")
+
