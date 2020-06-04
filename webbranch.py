d5b5301fd7bb287010c15edd497caafa539fcae8
Robel-RT <robelasgedom92@gmail.com>
2020-06-04 14:08:20 +0100

    makee some changes

--- webbranch.py
+++ webbranch.py
@@ -1,9 +1,10 @@
 import folium
 
-map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Mapbox Bright")
+map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
 
-map.add_child(folium.Marker(location=[38.2, -99.1], popup="Hi I am a Marker!", icon=folium.Icon(color='green')))
+fg=folium.FeatureGroup(name="My Group")
+fg.add_child(folium.Marker(location=[38.2, -99.1], popup="Hi I am a Marker!", icon=folium.Icon(color='green')))
+map.add_child(fg)
 
-
-map.save("Map2.html")
+map.save("Map1.html")
 
