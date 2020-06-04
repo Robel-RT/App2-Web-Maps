38ec004a69d029fe7339f49a6149578520501c2b
Robel-RT <robelasgedom92@gmail.com>
2020-06-04 13:44:05 +0100

    added child method, icon & popup

--- webbranch.py
+++ webbranch.py
@@ -1,4 +1,8 @@
 import folium
 
-map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="MapBox")
+map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Mapbox Bright")
+
+map.add_child(folium.Marker(location=[38.2, -99.1], popup="Hi I am a Marker!", icon=folium.Icon(color='green')))
+
+
 map.save("Map1.html")
